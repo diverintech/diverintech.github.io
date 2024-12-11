4f2ed54673682d85792c274e32bb9b1dd9d72f78
Joana <144175120+diverintech@users.noreply.github.com>
2024-12-11 15:48:33 +0000

    Add underconstruction page

---
+++ src/app/components/under-construction/under-construction.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-under-construction',
+  standalone: true,
+  imports: [CommonModule, TranslateModule],
+  templateUrl: './under-construction.component.html',
+  styleUrl: './under-construction.component.scss'
+})
+export class UnderConstructionComponent {
+
+}
