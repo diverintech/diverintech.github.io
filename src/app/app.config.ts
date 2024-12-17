fe7a03e9732723f7f394dff2db63128cb9da0a1b
Joana Pires <144175120+diverintech@users.noreply.github.com>
2024-12-17 10:30:18 +0000

    Update src/app/app.config.ts
    Co-authored-by: Rodrigo Silva <40007180+RodrasSilva@users.noreply.github.com>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
