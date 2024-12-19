d076897255a6d77507cfba074c3e833cd3e8ed0e
Joana <144175120+diverintech@users.noreply.github.com>
2024-12-19 11:33:31 +0000

    Create Home App

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { UnderConstructionComponent } from '../../components/under-construction/under-construction.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, TranslateModule, UnderConstructionComponent],
+  imports: [CommonModule, TranslateModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
